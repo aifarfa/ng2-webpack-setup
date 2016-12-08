e600f1803069a1eeaa04e187315450ed1df67763
pasit.r <pasit.rattanathammak@thomsonreuters.com>
2016-12-08 15:48:46 +0700

    add sample click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,68 @@
-import {Component} from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'my-app',
-  template: `
-    <div class="jumbotron">
-      <h2>Getting started.</h2>
-    </div>
+    selector: 'my-app',
+    template: `
+    <header>
+      <nav class="navbar navbar-inverse">
+        <div class="nvabar-header">
+          <a class="navbar-brand" href="/">My App</a>
+        </div>
+      </nav>
+    </header>
+    <main>
+      <div class="row">
+
+        <div class="col-sm-4">
+          <div *ngIf="users">
+            <ul class="list-group users-list">
+              <li class="list-group-item"
+                *ngFor="let user of users"
+                (click)="selectUser(user)">
+                {{user.name}} ({{user.username}})</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="col-sm-8">
+          <div class="jumbotron" *ngIf="activeUser">
+            <h2>{{activeUser.name}} <small>{{activeUser.username}}</small></h2>
+          </div>
+        </div>
+
+      </div>
+    </main>
+    <footer class="text-center">
+      Copy right 2016
+    </footer>
   `,
-  styles: [`
+    styles: [`
     .jumbotron {
       box-shadow: 0 2px 0 rgba(0, 0, 0, .2);
     }
+    .users-list > .list-group-item {
+      cursor: pointer
+    }
   `]
 })
-export class AppComponent {}
+
+export class AppComponent {
+    activeUser;
+
+    users = [{
+        id: 777, name: 'Far',
+        username: 'aifarfa'
+    }, {
+        id: 22,
+        name: 'John Doe',
+        username: 'john.d'
+    }, {
+        id: 24,
+        name: 'Unknown',
+        username: 'unknown'
+    }]
+
+    selectUser(user) {
+        this.activeUser = user;
+    }
+}
