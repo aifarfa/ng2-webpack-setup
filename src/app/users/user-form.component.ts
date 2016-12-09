f81d6844349b3a5a61d1d26883d1c57fb5ed9237
pasit.r <pasit.rattanathammak@thomsonreuters.com>
2016-12-09 17:52:36 +0700

    cleanup HTML

--- src/app/users/user-form.component.ts
+++ src/app/users/user-form.component.ts
@@ -9,32 +9,20 @@ import { User } from '../models/user'
     }
 
   `],
-  template: `
-    <form #form="ngForm" (ngSubmit)="onSubmit()">
-      <div class="form-group">
-        <input type="text" class="form-control" name="name" placeholder="name" required [(ngModel)]="user.name" #name="ngModel" />
-        <span class="help-block" *ngIf="name.invalid && name.touched">name is required</span>
-      </div>
-      <div class="form-group">
-        <input type="text" class="form-control" name="username" placeholder="username" required [(ngModel)]="user.username" #username="ngModel" />
-        <span class="help-block" *ngIf="username.invalid && username.touched">username is required</span>
-      </div>
-      <div class="form-group">
-        <button type="submit" class="btn btn-block btn-lg btn-primary" [disabled]="form.invalid">Create User</button>
-      </div>
-    </form>
-  `
+  templateUrl: './user-form.component.html'
 })
 export class UserFormComponent {
   @Output() userCreated = new EventEmitter()
 
+  active: boolean = true;
   user: User = new User();
 
   onSubmit(e) {
     this.userCreated.emit({
-       user: this.user
+      user: this.user
     })
-    // TODO: clear form
-    this.user = new User()
+    this.user = new User();
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
   }
 }
