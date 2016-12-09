06500cb0944c33ff8663b24ece0a4df1151423dc
pasit.r <pasit.rattanathammak@thomsonreuters.com>
2016-12-09 15:16:40 +0700

    refactor - separate html, css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,54 +3,8 @@ import { User } from './models/user'
 
 @Component({
     selector: 'my-app',
-    template: `
-    <header>
-      <nav class="navbar navbar-inverse">
-        <div class="nvabar-header">
-          <a class="navbar-brand" href="/">My App</a>
-        </div>
-      </nav>
-    </header>
-    <main>
-      <div class="row">
-
-        <div class="col-sm-4">
-          <div *ngIf="users">
-            <ul class="list-group users-list">
-              <li class="list-group-item"
-                *ngFor="let user of users"
-                (click)="selectUser(user)"
-                [class.active]="user === activeUser">
-                {{user.name}} ({{user.username}})</li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="col-sm-8">
-          <div class="jumbotron" *ngIf="activeUser">
-            <h2>{{activeUser.name}} <small>{{activeUser.username}}</small></h2>
-            <input class="form-control" [(ngModel)]="activeUser.name" />
-          </div>
-          <div class="jumbotron" *ngIf="!activeUser">
-            <h2>Choose a user.</h2>
-            <span class="glyphicon glyphicon-hand-left"></span>
-          </div>
-        </div>
-
-      </div>
-    </main>
-    <footer class="text-center">
-      Copy right 2016
-    </footer>
-  `,
-    styles: [`
-    .jumbotron .glyphicon {
-      font-size: 80px;
-    }
-    .users-list > .list-group-item {
-      cursor: pointer;
-    }
-  `]
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
